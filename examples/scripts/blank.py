9e91d3aaadb783632fec07795cdf3e429fffac48
Michael Franzl <office@michaelfranzl.com>
2016-04-26 13:59:16 +0200

    fixes

--- examples/scripts/blank.py
+++ examples/scripts/blank.py
@@ -4,12 +4,29 @@ c = compiler
 t = gcodetools
 grbl = self.grbl
 
-input = []
-input.append("G1X1000")
+gcodes = []
+
+gcodes.append("G55")
+
+gcodes.append("G0 X20 Y20")
+gcodes.append("G1 X30")
+gcodes.append("G1 X40")
+gcodes.append("G1 X50")
+gcodes.append("G2 X60 Y30 I5 J5")
+
+gcodes.append("G54")
+
+gcodes.append("G0 X20 Y20")
+gcodes.append("G1 X30")
+gcodes.append("G1 X40")
+gcodes.append("G1 X50")
+gcodes.append("G2 X60 Y30 I5 J5")
 
 self.new_job()
 
-self.grbl.write(input)
+self.grbl.preprocessor.do_fractionize_arcs = False
+self.grbl.preprocessor.do_fractionize_lines = False
+self.grbl.write(gcodes)
 
 self.set_target("simulator")
 self.job_run()
