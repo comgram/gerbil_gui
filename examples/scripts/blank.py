b41f72ac3f8f4c1dfbdd5ee79a83ed42ffb93067
Michael Franzl <office@michaelfranzl.com>
2016-04-28 10:51:32 +0200

    examples

--- examples/scripts/blank.py
+++ examples/scripts/blank.py
@@ -7,13 +7,14 @@ grbl = self.grbl
 gcodes = []
 
 gcodes.append(";blah")
-gcodes.append("G0 X20 Y20 Z20 ; nice command")
-gcodes.append("G1 X30")
+gcodes.append("G0 X0 Y0 Z0 ; nice command")
+gcodes.append("G1 X10 Y30")
+gcodes.append("G2 X20 Y40 I5 J5")
 
 self.new_job()
 
-self.grbl.preprocessor.do_fractionize_arcs = False
-self.grbl.preprocessor.do_fractionize_lines = False
+self.grbl.preprocessor.do_fractionize_arcs = True
+self.grbl.preprocessor.do_fractionize_lines = True
 self.grbl.write(gcodes)
 
 self.set_target("simulator")
--- examples/scripts/reload-file.py
+++ examples/scripts/reload-file.py
@@ -7,7 +7,7 @@ self.new_job()
 
 #gcode = t.read("tmp/patterntest2.ngc")
 gcode = t.read("/mnt/files/output.ngc")
-gcode = t.read("/home/michael/Documents/circuits/test1-top.gcode")
+#gcode = t.read("/home/michael/Documents/circuits/test1-top.gcode")
 
 grbl.preprocessor.do_fractionize_lines = False
 grbl.preprocessor.do_fractionize_arcs = False
